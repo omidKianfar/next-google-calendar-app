216cfa4dc427ae9d6c331fc16f9bf205974249f2
Omid Kianfar <kianfar.omid.990@gmail.com>
2025-11-02 19:37:03 +0330

    feat google calndar login & logout with get token from google calendar & full calendar

---
+++ src/components/pages/calendar/google-calendar/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useGoogleLogin, googleLogout } from "@react-oauth/google";
+import { useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { CalendarHeader } from "./header";
+
+export default function CalendarComponent() {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  const router = useRouter();
+
+  const login = useGoogleLogin({
+    flow: "implicit",
+    scope:
+      "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events openid email",
+    onSuccess: (res) => setAccessToken(res.access_token),
+    onError: (err) => console.error(err),
+  });
+
+  const handleLogout = () => {
+    googleLogout();
+    setAccessToken(null);
+    router.push("/calendar");
+  };
+
+  return (
+    <div className="p-6 min-h-screen bg-gray-50">
+      {!accessToken  ? (
+        <div className="flex justify-center items-center h-screen">
+          <button
+            onClick={() => login()}
+            className="bg-blue-500 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-600 transition"
+          >
+            Sign in with Google
+          </button>
+        </div>
+      ) : (
+        <>
+          <CalendarHeader onLogout={handleLogout} />
+
+          <div className="bg-white rounded-xl shadow p-4">
+            <FullCalendar
+              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+              initialView="dayGridMonth"
+              height="80vh"
+              selectable
+              headerToolbar={{
+                left: "prev,next today",
+                center: "title",
+                right: "dayGridMonth,timeGridWeek,timeGridDay",
+              }}
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
+}

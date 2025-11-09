052d6827447ad98af6f455d75da7ed9c339f3ca0
Omid Kianfar <kianfar.omid.990@gmail.com>
2025-11-09 20:09:13 +0330

    fix loading pages

---
+++ src/components/atom/loading/calendar-skeleton.tsx
@@ -0,0 +1,5 @@
+export const CalendarSkeleton = () => {
+  return (
+    <div className="w-full h-[80vh] rounded-xl bg-gray-200 animate-pulse" />
+  );
+};
---
+++ src/components/atom/loading/spinner.tsx
@@ -0,0 +1,7 @@
+export const LoadingSpinner = () => {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      <div className="animate-spin rounded-full h-10 w-10 border-4 border-gray-300 border-t-blue-500"></div>
+    </div>
+  );
+};

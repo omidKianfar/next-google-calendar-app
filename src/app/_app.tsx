f15c87d701c23a21d5a3da529c85e4257f3e363c
omid kianfar <=>
2025-11-28 14:33:28 +0330

    fix layots

--- src/app/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
-  const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
-
-  return getLayout(<Component {...pageProps} />);
-};
-
-export default MyApp;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import GoogleCalendarComponent from "@/components/pages/calendar";
 import Layout from "./layout";
 
-const CalendarPage: NextPageWithLayout = () => {
-  return <GoogleCalendarComponent />;
-};
-
-CalendarPage.getLayout = function Home(page) {
-  return <Layout>{page}</Layout>;
+const CalendarPage = () => {
+  return (
+    <Layout>
+      <GoogleCalendarComponent />
+    </Layout>
+  );
 };
 
 export default CalendarPage;
